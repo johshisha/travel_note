ddb3e7467d41998c5fa55cc894dbea0946f11e68
johshisha <johshisha@gmail.com>
2017-05-02 03:28:32 +0900

    Add other static_pages (temp)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
+  get 'home' => 'static_pages#home'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
